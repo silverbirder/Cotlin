acf04222f7c93e6b3a14be0f26b9741b2a5a6c45
shibamoto <silverbirder@gmail.com>
2020-03-02 18:16:36 +0900

    Delete unused crawler file

--- src/crawler/crawlerMock.ts
+++
@@ -1,5 +0,0 @@
-import ICrawler from "./iCrawler";
-
-export default class CrawlerMock implements ICrawler {
-
-}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import TwitterImpl from "./twitter/twitterImpl";
 import ControllerImpl from "./controller/controllerImpl";
-import CrawlerImpl from "./crawler/crawlerImpl";
 import ExtractorImpl from "./extractor/extractorImpl";
 
 declare const global: {
     Twitter: any
     Controller: any
-    Crawler: any
     Extractor: any
 };
 
@@ -16,12 +14,10 @@ export function greet() {
 
 global.Twitter = TwitterImpl;
 global.Controller = ControllerImpl;
-global.Crawler = CrawlerImpl;
 global.Extractor = ExtractorImpl;
 
 export {
     TwitterImpl as Twitter,
     ControllerImpl as Controller,
-    CrawlerImpl as Crawler,
     ExtractorImpl as Extractor
 }
