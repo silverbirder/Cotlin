23c9d4ba981ac3e7666ffcaf96b4cb0dd66335a2
shibamoto <silverbirder@gmail.com>
2020-03-06 07:35:30 +0900

    Add Jest Global object

--- __tests__/src/controller/controllerImpl.test.ts
+++ __tests__/src/controller/controllerImpl.test.ts
@@ -1,12 +1,7 @@
-import IController from "../../../src/controller/iController";
-import ControllerImpl from "../../../src/controller/controllerImpl";
-
 describe('Class: ControllerImpl', () => {
     describe('Method: TODO', () => {
         test('Assert: TODO', () => {
             // Arrange
-            const controller: IController = new ControllerImpl();
-
             // Act
             // Assert
         });
--- __tests__/src/twitter/twitterImpl.test.ts
+++ __tests__/src/twitter/twitterImpl.test.ts
@@ -1,9 +1,22 @@
+import ITwitter from "../../../src/twitter/iTwitter";
+import TwitterImpl from "../../../src/twitter/twitterImpl";
+
 describe('Class: TwitterImpl', () => {
     describe('Method: TODO', () => {
         test('Assert: TODO', () => {
+            // @ts-ignore
+            PropertiesService.getScriptProperties! = jest.fn(() => {
+                return {
+                    getProperty: jest.fn(() => {
+                    })
+                }
+            });
             // Arrange
+            const twitter: ITwitter = new TwitterImpl('', '', []);
             // Act
+
             // Assert
+            console.log(twitter);
         });
     })
 });
