7da9698f6edceb31dca5c1927771e4e5acd6e3df
shibamoto <silverbirder@gmail.com>
2020-03-05 20:22:56 +0900

    Add test code

--- __tests__/src/controller/controllerImpl.test.ts
+++ __tests__/src/controller/controllerImpl.test.ts
@@ -1,7 +1,12 @@
+import IController from "../../../src/controller/iController";
+import ControllerImpl from "../../../src/controller/controllerImpl";
+
 describe('Class: ControllerImpl', () => {
     describe('Method: TODO', () => {
         test('Assert: TODO', () => {
             // Arrange
+            const controller: IController = new ControllerImpl();
+
             // Act
             // Assert
         });
