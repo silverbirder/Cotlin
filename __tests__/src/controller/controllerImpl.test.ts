477a461972f10894c337eae302606304d9b377e0
shibamoto <silverbirder@gmail.com>
2020-03-10 07:35:09 +0900

    Add test code

--- __tests__/src/controller/controllerImpl.test.ts
+++ __tests__/src/controller/controllerImpl.test.ts
@@ -1,9 +1,50 @@
+import IController, {IParameter} from "../../../src/controller/iController";
+import ControllerImpl from "../../../src/controller/controllerImpl";
+import ITwitter from "../../../src/twitter/iTwitter";
+import TwitterMock from "../../../src/twitter/twitterMock";
+import IArchive from "../../../src/archive/iArchive";
+import ArchiveMock from "../../../src/archive/archiveMock";
+
 describe('Class: ControllerImpl', () => {
-    describe('Method: TODO', () => {
+    describe('Method: constructor', () => {
         test('Assert: TODO', () => {
             // Arrange
+            const twitter: ITwitter = new TwitterMock();
+            const archive: IArchive = new ArchiveMock();
+
             // Act
+            const controller: IController = new ControllerImpl(twitter, archive);
+
             // Assert
+            expect(controller).toBeTruthy();
+        });
+    });
+    describe('Method: parseParams', () => {
+        test('Assert: TODO', () => {
+            // Arrange
+            const twitter: ITwitter = new TwitterMock();
+            const archive: IArchive = new ArchiveMock();
+            const controller: IController = new ControllerImpl(twitter, archive);
+
+            // Act
+            const actualParesedParams: IParameter = controller.parseParams({parameter: {}});
+
+            // Assert
+            expect(actualParesedParams).toBeTruthy();
+        });
+    });
+    describe('Method: run', () => {
+        test('Assert: TODO', () => {
+            // Arrange
+            const twitter: ITwitter = new TwitterMock();
+            const archive: IArchive = new ArchiveMock();
+            const controller: IController = new ControllerImpl(twitter, archive);
+
+            // Act
+            const result:Array<any>  = controller.run();
+
+            // Assert
+            expect(result).toBeTruthy();
         });
     })
 });
